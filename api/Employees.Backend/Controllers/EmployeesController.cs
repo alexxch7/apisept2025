cab12c808d4f2064fa79b0c5ac5e90e19be76e5d
alexxch7 <alexanderchavarria290244@correo.itm.edu.co>
2025-10-10 23:50:03 -0500

    add /paginated, /totalRecords and /search endpoints to EmployeesController

--- api/Employees.Backend/Controllers/EmployeesController.cs
+++ api/Employees.Backend/Controllers/EmployeesController.cs
@@ -1,25 +1,40 @@
-﻿
-    using Employees.Backend.UnitsOfWork.Interfaces;
-    using Employees.Shared.Entities;
-    using Microsoft.AspNetCore.Mvc;
-    namespace Employees.Backend.Controllers;
+﻿using Employees.Backend.UnitsOfWork.Interfaces;
+using Employees.Shared.DTOs;
+using Employees.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
 
-
+namespace Employees.Backend.Controllers
+{
     [ApiController]
     [Route("api/[controller]")]
     public class EmployeesController : GenericController<Employee>
     {
-        private readonly IEmployeesUnitOfWork _employeesUoW;
+        private readonly IEmployeesUnitOfWork _uow;
 
-        public EmployeesController(IGenericUnitOfWork<Employee> genericUoW,
-                                   IEmployeesUnitOfWork employeesUoW) : base(genericUoW)
+        public EmployeesController(IEmployeesUnitOfWork uow) : base(uow)
         {
-            _employeesUoW = employeesUoW;
+            _uow = uow;
         }
+
         [HttpGet("search")]
-        public async Task<IActionResult> SearchAsync([FromQuery] string term)
+        public async Task<IActionResult> Search([FromQuery] string term)
         {
-            var resp = await _employeesUoW.SearchByNameAsync(term);
+            var resp = await _uow.SearchByNameAsync(term);
+            return resp.WasSuccess ? Ok(resp.Result) : BadRequest(resp.Message);
+        }
+
+        [HttpGet("paginated")]
+        public async Task<IActionResult> GetPaginated([FromQuery] PaginationDTO dto)
+        {
+            var resp = await _uow.GetAsync(dto);
+            return resp.WasSuccess ? Ok(resp.Result) : BadRequest(resp.Message);
+        }
+
+        [HttpGet("totalRecords")]
+        public async Task<IActionResult> GetTotalRecords([FromQuery] PaginationDTO dto)
+        {
+            var resp = await _uow.GetTotalRecordsAsync(dto);
             return resp.WasSuccess ? Ok(resp.Result) : BadRequest(resp.Message);
         }
     }
+}
